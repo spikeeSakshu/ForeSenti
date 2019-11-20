5f07250b197b9f969a806a4977113d546c20c7b7
spikeeSakshu <spikeesaksu@gmail.com>
2019-11-20 22:52:18 +0530

    Deleted and Updated files

--- get_twitter_data.py
+++ get_twitter_data.py
@@ -1,6 +1,7 @@
 import argparse
 import urllib
-import urllib3
+from urllib import parse
+#import urllib2
 import json
 import datetime
 import random
@@ -11,34 +12,45 @@ from datetime import timedelta
 import oauth2
 # from dateutil import parser
 
-
 class TwitterData:
     #start __init__
     def __init__(self,givenDate):
         self.currDate = datetime.datetime.strptime(givenDate,'%Y-%m-%d')
-  
+        #get date in this format
+        # self.currDate = datetime.datetime.now()
+        # print type(self.currDate)
+        # print self.currDate
+        # print type(y)
+        # print y
         self.weekDates = []
+        print('poorvi')
         self.weekDates.append(self.currDate.strftime("%Y-%m-%d"))
-        for i in range(1,10):
-            dateDiff = timedelta(days=-i)
+        #parse date in this format
+        for i in range(1,20):
+            dateDiff = datetime.timedelta(days=-i)
             newDate = self.currDate + dateDiff
             self.weekDates.append(newDate.strftime("%Y-%m-%d"))
+        # end loop
+        print("New Week Dates =",self.weekDates)
+    # end
 
     # start getWeeksData
     def getTwitterData(self, keyword, time):
         self.weekTweets = {}
+        print('poorvi1')
         if(time == 'lastweek'):
-            for i in range(0,9):
+            for i in range(0,19):
                 params = {'since': self.weekDates[i+1], 'until': self.weekDates[i]}
                 self.weekTweets[i] = self.getData(keyword, params)
             # end loop
 
             # Write data to a pickle file
-            filename = 'Data/weekTweets_'+urllib.unquote(keyword.replace("+", " "))+'_'+str(int(random.random()*10000))+'.txt'
+            filename = 'Data/weekTweets_'+urllib.parse.unquote(keyword.replace("+", " "))+'_'+str(int(random.random()*10000))+'.txt'
             outfile = open(filename, 'wb')
             pickle.dump(self.weekTweets, outfile)
             outfile.close()
         elif(time == 'today'):
+            print('poorvi2')
             for i in range(0,1):
                 params = {'since': self.weekDates[i+1], 'until': self.weekDates[i]}
                 # params = {'since': self.weekDates[i]}
@@ -59,63 +71,53 @@ class TwitterData:
       if os.path.exists('config.json'):
           with open('config.json') as f:
               config.update(json.load(f))
-      else:
-          # may be from command line
-          parser = argparse.ArgumentParser()
-
-          parser.add_argument('-ck', '--consumer_key', default=None, help='Your developper `Consumer Key`')
-          parser.add_argument('-cs', '--consumer_secret', default=None, help='Your developper `Consumer Secret`')
-          parser.add_argument('-at', '--access_token', default=None, help='A client `Access Token`')
-          parser.add_argument('-ats', '--access_token_secret', default=None, help='A client `Access Token Secret`')
-
-          args_ = parser.parse_args()
-          def val(key):
-            return config.get(key)\
-                   or getattr(args_, key)\
-                   or input('Your developper `%s`: ' % key)
-          config.update({
-            'consumer_key': val('consumer_key'),
-            'consumer_secret': val('consumer_secret'),
-            'access_token': val('access_token'),
-            'access_token_secret': val('access_token_secret'),
-          })
+    #   else:
+    #       args_ = parser.parse_args()
+    #       def val(key):
+    #         return config.get(key)\
+    #                or getattr(args_, key)\
+    #                or raw_input('Your developper `%s`: ' % key)
+    #       config.update({
+    #         'consumer_key': val('consumer_key'),
+    #         'consumer_secret': val('consumer_secret'),
+    #         'access_token': val('access_token'),
+    #         'access_token_secret': val('access_token_secret'),
+    #       })
       # should have something now
       return config
-
-    def oauth_req(self, url, http_method="GET", post_body=None,
+#
+    def oauth_req(self, url, http_method="GET", post_body='',
                   http_headers=None):
       config = self.parse_config()
+      print(config.get('consumer_key'))
       consumer = oauth2.Consumer(key=config.get('consumer_key'), secret=config.get('consumer_secret'))
       token = oauth2.Token(key=config.get('access_token'), secret=config.get('access_token_secret'))
       client = oauth2.Client(consumer, token)
-
-      resp, content = client.request(
-          url,
-          method=http_method,
-          body=post_body or '',
-          headers=http_headers
-      )
+      print('token accepted')
+      resp, content = client.request(url,method=http_method,body=bytes('', 'utf-8'),headers=http_headers)
+      
       return content
 
     # start getTwitterData
-    def getData(self, keyword= '#APPLE', params = {}):
+    def getData(self, keyword, params = {}):
         maxTweets = 200
         url = 'https://api.twitter.com/1.1/search/tweets.json?'
-        data = {'q': keyword, 'lang': 'en', 'result_type': 'mixed', 'since_id': 2014,'count': maxTweets, 'include_entities': 0}
-
+        data = {'q': keyword, 'lang': 'en', 'result_type': 'mixed', 'since_id': 2019,'count': maxTweets, 'include_entities': 0}
+        print('hello')
         # Add if additional params are passed
         if params:
             for key, value in params.iteritems():
                 data[key] = value
-
+        print(url)
         url += urllib.parse.urlencode(data)
-
+        #url=urllib.parse.urlencode(url)
+        print(type(url))
         response = self.oauth_req(url)
         jsonData = json.loads(response)
         tweets = []
         if 'errors' in jsonData:
             print ("API Error")
-            print (jsonData['errors'])
+            print(jsonData['errors'])
         else:
             for item in jsonData['statuses']:
                 #print item['created_at']
@@ -128,6 +130,5 @@ class TwitterData:
     # end
 
 # end class
-
-th= TwitterData('2019-11-15')
-print(th.getData())
+obj=TwitterData('2019-11-18')
+obj.getData('stock')
